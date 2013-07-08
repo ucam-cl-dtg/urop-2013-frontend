2e07e6584bbf3a6e591aa404dc340dcaf160c5bd
Jack Dent <jack@jackdent.co.uk>
2013-07-08 19:57:24 +0100

    Mobile navigation and sidebar bugs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
 
 	//
+	// Functions
+	//
+
+	function resizeSidebar() {
+		$('.sidebar').height($('.central-content').height());	
+	}
+
+	function updateMainContent(templateName) {
+		$.get('templates/' + templateName + '.html', function(data) {
+			$('.main').html(data);
+			resizeSidebar();
+		}).fail(function() {
+			alert('There was an error loading the content');
+		});
+	}
+
+	//
 	// Event handlers
 	//
+
+	$('.mobile-nav-options-button').on('click', function() {
+		$('.mobile-nav-wrapper').slideToggle();
+	});
 
 	$('.sidebar-navigation-item-header').on('click', function() {
 		$(this).next('.sidebar-sub-navigation').slideToggle();
@@ -17,18 +38,9 @@ $(document).ready(function() {
 		updateMainContent('notification-feed');
 	});
 
-	//
-	// Functions
-	//
-
-	function updateMainContent(templateName) {
-		$.get('templates/' + templateName + '.html', function(data) {
-			$('.main').html(data);
-		}).fail(function() {
-			alert('There was an error loading the content');
-		});
-	}
+	$(window).resize(function() {
+		resizeSidebar();
+	});
 
 	updateMainContent('notification-feed');
-
 });
