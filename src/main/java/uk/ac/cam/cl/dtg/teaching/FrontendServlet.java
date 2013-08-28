bd62206f17137f66d7b3029524daac6d5c3d4dfe
Andrew Rice <acr31@cam.ac.uk>
2013-08-28 11:07:14 +0100

    Added more error detection code to the frontend to try and make our error messages a bit more descriptive when things don't work

--- src/main/java/uk/ac/cam/cl/dtg/teaching/FrontendServlet.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/FrontendServlet.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.cl.dtg.teaching;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Arrays;
 
 import javax.servlet.ServletConfig;
@@ -12,10 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.UriBuilder;
 
 import org.jboss.resteasy.client.ClientRequestFactory;
+import org.jboss.resteasy.client.ClientResponseFailure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.ac.cam.cl.dtg.teaching.api.DashboardApi;
+import uk.ac.cam.cl.dtg.teaching.api.DashboardApi.Settings;
 
 import com.google.common.collect.ImmutableMap;
 
@@ -31,6 +34,11 @@ public class FrontendServlet extends HttpServlet {
 	private String[] cssFiles;
 	private String[] jsFiles;
 
+	private void raiseInitError(String message) throws ServletException {
+		log.error(message);
+		throw new ServletException(message);
+	}
+	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
@@ -38,13 +46,22 @@ public class FrontendServlet extends HttpServlet {
 		// Load dashboard API URL from servlet context.
 		dashboardUrl = config.getServletContext().getInitParameter("dashboardUrl");
 
+		if (dashboardUrl == null) {
+			raiseInitError("Missing dashboardUrl from context parameters");
+		}
+		
+		URI dashboardUri = UriBuilder.fromUri(dashboardUrl).build();
+		if (dashboardUri.getHost() == null) {
+			raiseInitError("Couldn't find a host when parsing dashboardUrl "+dashboardUrl);
+		}
+		
 		cssNamespace = config.getServletContext().getInitParameter("cssNamespace");
 
 		// Load global API key from servlet context for accessing dashboard.
 		apiKey = config.getServletContext().getInitParameter("apiKey");
 		
 		if(apiKey == null) {
-			log.error("Missing API key from context parameters.");
+			raiseInitError("Missing API key from context parameters.");
 		}
 		
 		// CSS files to include
@@ -93,8 +110,16 @@ public class FrontendServlet extends HttpServlet {
 			return;
 		}
 		
-		ClientRequestFactory crf = new ClientRequestFactory(UriBuilder.fromUri(dashboardUrl).build());
+		URI dashboardUri = UriBuilder.fromUri(dashboardUrl).build();
+		ClientRequestFactory crf = new ClientRequestFactory(dashboardUri);
 		String userId = (String) req.getSession().getAttribute("RavenRemoteUser");
+		
+		Settings settings;
+		try {
+			settings = crf.createProxy(DashboardApi.class).getSettings(userId, apiKey);
+		} catch (ClientResponseFailure e) {
+			throw new ServletException("Couldn't load Settings from "+dashboardUri);
+		}
 		
 		// Load extra javascript/CSS as required.
 		req.setAttribute("model", ImmutableMap.of(
@@ -102,7 +127,7 @@ public class FrontendServlet extends HttpServlet {
 			"cssFiles",     Arrays.asList(cssFiles),
 			"jsFiles",      Arrays.asList(jsFiles),
 			"contextPath",  req.getContextPath(),
-			"settings", 	crf.createProxy(DashboardApi.class).getSettings(userId, apiKey)
+			"settings", 	settings
 		));
 
 		// Direct all requests to the main frontend template via Silken.
