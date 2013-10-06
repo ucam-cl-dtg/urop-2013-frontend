8d8e5f36921c0847b7f71eeb96c56fe0f6d56eee
Andrew Rice <acr31@cam.ac.uk>
2013-10-06 19:56:00 +0100

    Removed unused variable, moved log level down to DEBUG because it was too noisy and reformatted the file for consistent layout

--- src/main/java/uk/ac/cam/cl/dtg/teaching/ResourceFilter.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/ResourceFilter.java
@@ -10,7 +10,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,24 +19,25 @@ public class ResourceFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig config) throws ServletException {
-			log.info("Initialised Resource Filter");
-		}
+		log.info("Initialised Resource Filter");
+	}
 
 	@Override
-	public void doFilter(ServletRequest servletReq, ServletResponse servletResp,
-			FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest servletReq,
+			ServletResponse servletResp, FilterChain chain) throws IOException,
+			ServletException {
 
 		// Convert arguments to HTTP ones to allow grabbing session etc.
 		HttpServletRequest request = (HttpServletRequest) servletReq;
 		HttpServletResponse response = (HttpServletResponse) servletResp;
-		HttpSession session = request.getSession();
 
 		String path = request.getRequestURI();
-		if(path!=null){
-			log.info("Stopping filter chain for " + path);
+		if (path != null) {
+			log.debug("Stopping filter chain for {}", path);
 			String context = request.getContextPath();
-			path = path.replaceFirst(context, ""); // Trim context path from request forwarder
-					
+			path = path.replaceFirst(context, ""); // Trim context path from
+													// request forwarder
+
 			request.getRequestDispatcher(path).forward(request, response);
 		}
 	}
@@ -47,4 +47,3 @@ public class ResourceFilter implements Filter {
 		// Nothing to do
 	}
 }
-
