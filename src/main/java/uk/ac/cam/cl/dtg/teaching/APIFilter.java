ae157e0cbb4a92cc3748793e3f5961abbaebc11f
Andrew Rice <acr31@cam.ac.uk>
2013-10-06 19:56:40 +0100

    Substantial rewrite to use the new generic hibernate tools and to make the authorization flow clearer

--- src/main/java/uk/ac/cam/cl/dtg/teaching/APIFilter.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/APIFilter.java
@@ -2,7 +2,6 @@ package uk.ac.cam.cl.dtg.teaching;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,18 +12,33 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.jboss.resteasy.client.ClientRequestFactory;
+import org.jboss.resteasy.client.ClientResponseFailure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.ac.cam.cl.dtg.teaching.api.ApiFailureMessage;
 import uk.ac.cam.cl.dtg.teaching.api.DashboardApi;
 import uk.ac.cam.cl.dtg.teaching.api.DashboardApi.ApiPermissions;
+import uk.ac.cam.cl.dtg.teaching.hibernate.HibernateUtil;
 
 public class APIFilter implements Filter {
-	private static Logger log = LoggerFactory.getLogger(APIFilter.class);
+
+	private static final String INIT_PARAM_ALLOW_GLOBAL = "allowGlobal";
+
+	private static final String INIT_PARAM_EXCLUDE_FROM_LOGGER = "excludeFromLogger";
+
+	private static final String INIT_PARAM_EXCLUDE_PREFIXES = "excludePrefixes";
+
+	private static final String SERVLET_INIT_PARAM_API_KEY = "apiKey";
+
+	private static final String SERVLET_INIT_PARAM_DASHBOARD_URL = "dashboardUrl";
 
 	/**
 	 * Name of the request attribute populated with the current user.
@@ -52,177 +66,236 @@ public class APIFilter implements Filter {
 	private String apiKey;
 
 	/**
-	 * Whether the service supports global API keys (defaults to true).
+	 * Whether the service supports global API keys (defaults to false).
 	 */
 	private boolean allowGlobal = true;
 
+	private static Logger log = LoggerFactory.getLogger(APIFilter.class);
+	
+	private HibernateUtil hibernateUtil;
+
 	@Override
 	public void init(FilterConfig config) throws ServletException {
-		// Load dashboard API URL from servlet context.
-		dashboardUrl = config.getServletContext().getInitParameter("dashboardUrl");
 
-		if(dashboardUrl == null) {
+		hibernateUtil = HibernateUtil.init("/hibernateRequestLog.cfg.xml");
+
+		dashboardUrl = config.getServletContext().getInitParameter(
+				SERVLET_INIT_PARAM_DASHBOARD_URL);
+		if (dashboardUrl == null) {
 			log.error("Missing dashboard URL from context parameters.");
 		}
 
-		// Load dashboard API URLs to exclude from filter from servlet context.
-		// Trims blank space
-		String prefixes = config.getServletContext().getInitParameter("excludePrefixes");
-		if(prefixes != null) {
-			excludePrefixes = prefixes.split(",");
+		this.excludePrefixes = getInitParamList(config,
+				INIT_PARAM_EXCLUDE_PREFIXES);
+		this.excludeFromLogger = getInitParamList(config,
+				INIT_PARAM_EXCLUDE_FROM_LOGGER);
 
-			for (int i = 0; i < excludePrefixes.length; i++) {
-				excludePrefixes[i] = excludePrefixes[i].trim();
-			}
-		}
-
-		// Load API URLs to exclude from logger from servlet context.
-		// Trims blank space
-		String exclude = config.getServletContext().getInitParameter("excludeFromLogger");
-		if(exclude != null) {
-			excludeFromLogger = exclude.split(",");
-
-			for (int i = 0; i < excludeFromLogger.length; i++) {
-				excludeFromLogger[i] = excludeFromLogger[i].trim();
-			}
-		}
-
-		// Load global API key from servlet context for accessing dashboard.
-		apiKey = config.getServletContext().getInitParameter("apiKey");
-
-		if(apiKey == null) {
+		apiKey = config.getServletContext().getInitParameter(
+				SERVLET_INIT_PARAM_API_KEY);
+		if (apiKey == null) {
 			log.error("Missing API key from context parameters.");
 		}
 
-		// Determine whether service supports global API key.
-		String sAllowGlobal = config.getInitParameter("allowGlobal");
-
-		if(sAllowGlobal != null) {
-			if(sAllowGlobal.toLowerCase().equals("false")) {
-				allowGlobal = false;
-			} else if(sAllowGlobal.toLowerCase().equals("true")) {
-				allowGlobal = true;
-			} else {
-				log.warn("allowGlobal init-param should either be 'true' or 'false'.");
-			}
-		}
+		allowGlobal = Boolean.valueOf(config
+				.getInitParameter(INIT_PARAM_ALLOW_GLOBAL));
 
 		log.info("API filter initialised.");
 	}
 
-	private static boolean isSessionValid(HttpSession session) {
-		try {
-			session.getCreationTime();
-			return true;
-		}
-		catch (IllegalStateException e) {
-			return false;
-		}
+	@Override
+	public void destroy() {
+		hibernateUtil.close();
 	}
 
 	@Override
-	public void doFilter(ServletRequest servletReq, ServletResponse servletResp,
-			FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest servletReq,
+			ServletResponse servletResp, FilterChain chain) throws IOException,
+			ServletException {
 
 		// Convert arguments to HTTP ones to allow grabbing session etc.
 		HttpServletRequest request = (HttpServletRequest) servletReq;
 		HttpServletResponse response = (HttpServletResponse) servletResp;
 		HttpSession session = request.getSession();
 
-		// Check whether the URL should be excluded from the filter
-		// If so, chain through
-		if(excludePrefixes != null) {
-			for (String p:excludePrefixes) {
-				if(request.getRequestURI().startsWith(request.getContextPath() + p)) {
-					log.info("Chaining request through API filter with prefix: " + request.getRequestURI());
-					chain.doFilter(request, response);
-					return;
-				}
-			}
-		}
+		try {
+			boolean authorized = false;
 
-		// API key provided.
-		if(request.getParameter("key") != null) {
-			String key = (String) request.getParameter("key");
-
-			ClientRequestFactory crf = new ClientRequestFactory(UriBuilder.fromUri(dashboardUrl).build());
-			DashboardApi dApi = crf.createProxy(DashboardApi.class);
-			ApiPermissions permissions;
-
-			try {
-				permissions = dApi.getApiPermissions(key);
-			} catch(Exception e) {
-				log.error("Error checking key: " + e.getMessage());
-				response.sendError(500, "Error checking key");
-				return;
+			if (matchesExcludedPrefix(request)) {
+				authorized = true;
+			} else if ((request.getParameter("key") != null)
+					&& checkKey(request)) {
+				authorized = true;
+			} else if (isSessionValid(session)
+					&& session.getAttribute("RavenRemoteUser") != null) {
+				// We have a Raven login active
+				String crsid = (String) session.getAttribute("RavenRemoteUser");
+				log.debug("API request permitted for user " + crsid);
+				logRequest(crsid, request.getRequestURI().toString(),
+						request.getMethod(), request.getQueryString());
+				request.setAttribute(USER_ATTR, crsid);
+				authorized = true;
 			}
 
-			// Empty response
-			if(permissions == null) {
-				log.error("Error checking key: empty response");
-				response.sendError(500, "Error checking key");
-			// Key was invalid
-		    } else if(permissions.getError() != null) {
-				log.error("Request with invalid API key = " + key);
-				response.sendError(401, "Invalid API key.");
-			// Global key
-			} else if(permissions.getType().equals("global")) {
-				// Global supported, allow request with null user.
-                // Allow global api keys to fake users.
-                String fakeUser = request.getParameter("impostorUser");
-				if(allowGlobal) {
-					log.debug("API request permitted for global key.");
-					logRequest("GLOBAL", request.getRequestURI().toString(), request.getMethod(), request.getQueryString());
-					request.setAttribute(USER_ATTR, fakeUser);
-					chain.doFilter(request, response);
-				// Global unsupported, return 405 (unsupported method).
-				} else {
-					log.error("Request with global API key when allowGlobal=false.");
-					response.sendError(405, "Global API keys unsupported.");
-				}
-			// User-specific key.
-			} else if(permissions.getType().equals("user")) {
-				String userId = permissions.getUserId();
-
-				log.debug("API request permitted with key for " + userId);
-				logRequest(userId, request.getRequestURI().toString(), request.getMethod(), request.getQueryString());
-			  request.setAttribute(USER_ATTR, userId);
+			if (authorized) {
 				chain.doFilter(request, response);
+			} else {
+				throw new AuthFailException(new ApiFailureMessage(
+						"Failed to authorize request.  Key provided was: "
+								+ request.getParameter("key")));
 			}
-		} else if (!isSessionValid(session)) {
-			// an earlier filter invalidates sessions if the user has logged out (for example)
-			response.sendError(401, "Unauthorised API request.");
-		// Check whether we're logged in with Raven.
-		} else if(session.getAttribute("RavenRemoteUser") != null) {
-			String crsid = (String) session.getAttribute("RavenRemoteUser");
+		} catch (AuthFailException e) {
 
-			log.debug("API request permitted for user " + crsid);
-
-			logRequest(crsid, request.getRequestURI().toString(), request.getMethod(), request.getQueryString());
-			request.setAttribute(USER_ATTR, crsid);
-			chain.doFilter(request, response);
-		// No other authorisation options.
-		} else {
-			response.sendError(401, "Unauthorised API request.");
+			ObjectMapper mapper = new ObjectMapper();
+			String jsonString = mapper.writeValueAsString(e.message);
+			response.setContentType(MediaType.APPLICATION_JSON);
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().write(jsonString);
 		}
 
 	}
 
-	@Override
-	public void destroy() {
-		// Nothing to do
+	private boolean matchesExcludedPrefix(HttpServletRequest request) {
+		if (excludePrefixes != null) {
+			// Check whether the URL should be excluded from the filter
+			// If so, chain through
+			for (String p : excludePrefixes) {
+				if (request.getRequestURI().startsWith(
+						request.getContextPath() + p)) {
+					log.info("Chaining request through API filter with prefix: "
+							+ request.getRequestURI());
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
-	private void logRequest(String crsid, String uri, String method, String queryString) {
+	private static class AuthFailException extends Exception {
 
-		if (excludeFromLogger != null && !Arrays.asList(excludeFromLogger).contains(uri) || excludeFromLogger == null) {
-			Session s = HibernateUtil.getTransactionSession();
+		private static final long serialVersionUID = -5530143141307492684L;
+		ApiFailureMessage message;
 
-			RequestLog rl = new RequestLog(crsid, uri, queryString, method);
+		public AuthFailException(ApiFailureMessage message) {
+			super(message.getMessage());
+			this.message = message;
+		}
+	}
 
-			s.save(rl);
-			HibernateUtil.commit();
+	private boolean checkKey(HttpServletRequest request)
+			throws AuthFailException {
+		String key = (String) request.getParameter("key");
+
+		ApiPermissions permissions = lookupApiPermissions(key);
+
+		if (permissions.getError() != null) {
+			// Key was invalid
+			log.error("Request with invalid API key = " + key);
+			// TODO: return SC 401
+			throw new AuthFailException(new ApiFailureMessage(
+					"The API key provided was invalid", new ApiFailureMessage(
+							permissions.getError())));
 		}
 
+		if (permissions.getType().equals("global")) {
+			String fakeUser = request.getParameter("impostorUser");
+
+			if (allowGlobal) {
+				// Global supported, allow request with null user.
+				// Allow global api keys to fake users.
+
+				log.debug("API request permitted for global key.");
+				logRequest("GLOBAL", request.getRequestURI().toString(),
+						request.getMethod(), request.getQueryString());
+				request.setAttribute(USER_ATTR, fakeUser);
+
+				return true;
+			} else {
+				// Global unsupported,
+				// TODO: return 405 (unsupported method)
+				log.error("Request with global API key when allowGlobal=false.");
+				throw new AuthFailException(new ApiFailureMessage(
+						"Global API keys are not supported by this method"));
+			}
+		}
+
+		if (permissions.getType().equals("user")) {
+			// User-specific key.
+			String userId = permissions.getUserId();
+
+			log.debug("API request permitted with key for " + userId);
+			logRequest(userId, request.getRequestURI().toString(),
+					request.getMethod(), request.getQueryString());
+			request.setAttribute(USER_ATTR, userId);
+			return true;
+		}
+
+		return false;
+	}
+
+	private ApiPermissions lookupApiPermissions(String key)
+			throws AuthFailException {
+		ClientRequestFactory crf = new ClientRequestFactory(UriBuilder.fromUri(
+				dashboardUrl).build());
+		DashboardApi dApi = crf.createProxy(DashboardApi.class);
+		try {
+			ApiPermissions permissions = dApi.getApiPermissions(key);
+			if (permissions == null) {
+				// Empty response
+				log.error("Error checking key: empty response");
+				throw new AuthFailException(
+						new ApiFailureMessage(
+								"The returned API permissions information was null when checking key"));
+			}
+			return permissions;
+		} catch (ClientResponseFailure e) {
+			@SuppressWarnings("unchecked")
+			ApiFailureMessage message = (ApiFailureMessage) e.getResponse()
+					.getEntity(ApiFailureMessage.class);
+			throw new AuthFailException(
+					new ApiFailureMessage(
+							"Failed to read API permissions when checking key",
+							message));
+		}
+	}
+
+	private void logRequest(String crsid, String uri, String method,
+			String queryString) throws AuthFailException {
+		
+		Session s = null;
+		try {
+			if (excludeFromLogger != null
+					&& !Arrays.asList(excludeFromLogger).contains(uri)
+					|| excludeFromLogger == null) {
+				s = hibernateUtil.getSession();
+				RequestLog rl = new RequestLog(crsid, uri, queryString, method);
+				s.save(rl);
+				s.getTransaction().commit();
+			}
+		} catch (HibernateException e) {
+			if (s != null) s.getTransaction().rollback();
+			throw new AuthFailException(new ApiFailureMessage(
+					"A database problem occurred when logging request", e));
+		}
+	}
+
+	private static boolean isSessionValid(HttpSession session) {
+		try {
+			session.getCreationTime();
+			return true;
+		} catch (IllegalStateException e) {
+			return false;
+		}
+	}
+
+	private static String[] getInitParamList(FilterConfig config, String name) {
+		String s = config.getServletContext().getInitParameter(name);
+		if (s != null) {
+			String[] list = s.split(",");
+			for (int i = 0; i < list.length; i++) {
+				list[i] = list[i].trim();
+			}
+			return list;
+		}
+		return null;
 	}
 }
